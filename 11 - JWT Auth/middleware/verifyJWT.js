a025e27ac67cb3501b5bf84df5526b65f6c7b826
TomKingori <tomkingori66@gmail.com>
2024-03-28 20:38:10 +0300

    JTW authentication

---
+++ 11 - JWT Auth/middleware/verifyJWT.js
@@ -0,0 +1,16 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.sendStatus(401);
+  console.log(authHeader); // Bearer token
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); //Invalid token
+    req.user = decoded.username;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
